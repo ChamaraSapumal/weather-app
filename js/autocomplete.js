cb5219fd5fb19e3938aa6c1579c3a41e1c99f705
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-04-30 19:54:45 +0530

    add jQuery & add autocomplete

---
+++ js/autocomplete.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#cityInput").autocomplete({
+    source: function (request, response) {
+      fetch(
+        "http://api.weatherapi.com/v1/search.json?key=f855bd7413894855a3c85713233012&q=" +
+          request.term
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          var cities = data.map((item) => item.name);
+          response(cities);
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+    },
+    minLength: 2,
+  });
+});
