cceb56f0ad405fc3cc263653ae988f1e8323d1ca
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-05-02 07:44:40 +0530

    add forecast for 24 hours

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -37,6 +37,47 @@ function currentLocationWeather(latitude, longitude) {
         data.forecast.forecastday[0].day.maxtemp_c + "&deg;";
       document.getElementById("low").innerHTML =
         data.forecast.forecastday[0].day.mintemp_c + "&deg;";
+      document.getElementById("current-date").innerHTML =
+        data.current.last_updated;
+
+      // Remove existing hourly forecast list items
+      const hourlyList = document.getElementById("hourly-list");
+      hourlyList.innerHTML = "";
+
+      const currentHour = new Date().getHours();
+
+      // Create list items for hourly forecast
+      for (let i = 0; i < 24; i++) {
+        const listItem = document.createElement("li");
+        listItem.classList.add("me-auto");
+
+        if (i === currentHour) {
+          listItem.classList.add("bg-info");
+          listItem.classList.add("bg-opacity-10");
+          listItem.classList.add("border");
+          listItem.classList.add("border-info");
+
+          listItem.scrollIntoView({ behavior: "smooth", block: "nearest" });
+        }
+        const div = document.createElement("div");
+        div.classList.add("border-end");
+        const hour = document.createElement("h6");
+        hour.classList.add("h6", "text-center", "mx-4");
+        hour.textContent = ("0" + i).slice(-2); // format hour with leading zero
+        const img = document.createElement("img");
+        img.classList.add("rounded");
+        img.alt = "Weather Icon";
+        img.src = data.forecast.forecastday[0].hour[i].condition.icon;
+        const temp = document.createElement("h6");
+        temp.classList.add("h6", "text-center", "mx-4");
+        temp.innerHTML = data.forecast.forecastday[0].hour[i].temp_c + "&deg;";
+
+        div.appendChild(hour);
+        div.appendChild(img);
+        div.appendChild(temp);
+        listItem.appendChild(div);
+        hourlyList.appendChild(listItem);
+      }
     })
     .catch((error) => console.error("Error fetching weather:", error));
 }
